63d1a99f0ec1d253f55f7ec5cde8e85b07c94845
grraoo <evil@grraoo.ru>
2018-04-23 14:58:28 +0300

    svg-persons and mobile menu

--- snwall.site/build/js/main.js
+++ snwall.site/build/js/main.js
@@ -3,6 +3,23 @@ var main = (function () {
 
 var main = 0;
 
+const menu = document.querySelector(`.main-menu`);
+const menuToggle = menu.querySelector(`.main-menu__toggle`);
+
+const menuItems = menu.querySelector(`.main-menu__items`);
+const menuHeight = menuItems.offsetHeight;
+menuItems.style = `max-height: 0`;
+
+
+menuToggle.addEventListener(`click`, () => {
+  menu.classList.toggle(`main-menu--active`);
+  if (menu.classList.contains(`main-menu--active`)) {
+    menuItems.style = `max-height: ${menuHeight}px`;
+  } else {
+    menuItems.style = `max-height: 0`;
+  }
+});
+
 return main;
 
 }());
--- snwall.site/js/main.js
+++ snwall.site/js/main.js
@@ -1,1 +1,18 @@
 export default 0;
+
+const menu = document.querySelector(`.main-menu`);
+const menuToggle = menu.querySelector(`.main-menu__toggle`);
+
+const menuItems = menu.querySelector(`.main-menu__items`);
+const menuHeight = menuItems.offsetHeight;
+menuItems.style = `max-height: 0`;
+
+
+menuToggle.addEventListener(`click`, () => {
+  menu.classList.toggle(`main-menu--active`);
+  if (menu.classList.contains(`main-menu--active`)) {
+    menuItems.style = `max-height: ${menuHeight}px`;
+  } else {
+    menuItems.style = `max-height: 0`;
+  }
+});

87981fa3a3e04f8ad847ea23171df3b0d8717d9c
grraoo <evil@grraoo.ru>
2018-05-03 12:44:33 +0300

    start screen ready

--- snwall.site/build/js/main.js
+++ snwall.site/build/js/main.js
@@ -5,11 +5,10 @@ var main = 0;
 
 const menu = document.querySelector(`.main-menu`);
 const menuToggle = menu.querySelector(`.main-menu__toggle`);
-
 const menuItems = menu.querySelector(`.main-menu__items`);
 const menuHeight = menuItems.offsetHeight;
-menuItems.style = `max-height: 0`;
 
+menuItems.style = `max-height: 0`;
 
 menuToggle.addEventListener(`click`, () => {
   menu.classList.toggle(`main-menu--active`);

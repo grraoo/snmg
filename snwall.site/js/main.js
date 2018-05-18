b74cdddb53dd8c9dc807cbbbdc70fed18859a870
grraoo <evil@grraoo.ru>
2018-05-18 14:46:16 +0300

    scrollbtn + more adaptive

--- snwall.site/js/main.js
+++ snwall.site/js/main.js
@@ -2,8 +2,9 @@ import advSlider from "./slider/slider";
 import initMenu from "./menu/menu";
 import runScroller from "./scroll/run-scroller";
 
+runScroller();
+
 initMenu();
 
 advSlider.init();
 
-runScroller();
--- snwall.site/js/menu/menu.js
+++ snwall.site/js/menu/menu.js
@@ -7,6 +7,35 @@ const menuToggle = menu.querySelector(`.main-menu__toggle`);
 const menuItems = menu.querySelector(`.main-menu__items`);
 const menuHeight = menuItems.offsetHeight;
 
+const units = [...document.querySelectorAll(`.unit`)].map((unit) => {
+  return {
+    element: unit,
+    id: `#${unit.id}`,
+    top: () => unit.offsetTop
+  };
+}).reverse();
+let currentUnit;
+
+const reActivateLink = (link) => {
+  const currentLInk = document.querySelector(`.main-menu__link--active`);
+  if (currentLInk) {
+    currentLInk.classList.remove(`main-menu__link--active`);
+  }
+  link.classList.add(`main-menu__link--active`);
+};
+
+window.addEventListener(`scroll`, (e) => {
+
+  let tempCurrentUnit = units.find((unit) => unit.top() <= document.documentElement.scrollTop + 50);
+  if (currentUnit !== tempCurrentUnit) {
+    currentUnit = tempCurrentUnit;
+    const link = document.querySelector(`a[href="${currentUnit.id}"]`);
+    if (link) {
+      reActivateLink(link);
+    }
+  }
+});
+
 const switchMenu = () => {
   menu.classList.toggle(`main-menu--active`);
   if (menu.classList.contains(`main-menu--active`)) {
@@ -15,24 +44,19 @@ const switchMenu = () => {
     menuItems.style = `max-height: 0`;
   }
 };
+const scrollBtn = document.getElementById(`scroll-down`);
 
 const runSmoothScroll = (e) => {
-  if (e.target.classList.contains(`main-menu__link`) && e.target.getAttribute(`href`).length > 1) {
+  if (e.target.getAttribute(`href`).length > 1) {
     e.preventDefault();
     const element = document.querySelector(e.target.getAttribute(`href`));
-    smoothScrollTo(document.documentElement, element.offsetTop, SCROLL_TIME).then(() => {
-      const currentLInk = document.querySelector(`.main-menu__link--active`);
-      if (currentLInk) {
-        currentLInk.classList.remove(`main-menu__link--active`);
-      }
-      e.target.classList.add(`main-menu__link--active`);
-    });
+    smoothScrollTo(document.documentElement, element.offsetTop, SCROLL_TIME).then(switchMenu).catch();
   }
 };
-
 export default () => {
   menuItems.style = `max-height: 0`;
 
   menu.addEventListener(`click`, runSmoothScroll);
   menuToggle.addEventListener(`click`, switchMenu);
+  scrollBtn.addEventListener(`click`, runSmoothScroll);
 };
